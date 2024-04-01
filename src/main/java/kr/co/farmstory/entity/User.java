27fe35771def6a6ba497852c2c3eb0dab36462e9
gahui446 <gahui446@gmail.com>
2024-04-01 11:25:49 +0900

    기본 패키지 구성

---
+++ src/main/java/kr/co/farmstory/entity/User.java
@@ -0,0 +1,26 @@
+package kr.co.farmstory.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    private String uid;
+    private String role;
+    private String pass;
+
+
+}
