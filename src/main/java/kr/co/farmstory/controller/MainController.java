3093aea243c0ec7f442bb8f1d869c0627d8cea67
kkome <exazunyan1@gmail.com>
2024-04-01 17:20:46 +0900

    로그인, 회원가입 by everybody24

--- src/main/java/kr/co/farmstory/controller/MainController.java
+++ src/main/java/kr/co/farmstory/controller/MainController.java
@@ -1,4 +1,18 @@
 package kr.co.farmstory.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
 public class MainController {
+
+    @GetMapping(value={"/", "/index"})
+    public String index(Authentication authentication){
+        return "/index";
+    }
 }
