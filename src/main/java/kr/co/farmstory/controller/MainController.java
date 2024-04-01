6f0f2a42b852abe4e49404bda5d5d22b502c5c3b
hyeji111544 <hyeji111544@gmail.com>
2024-04-01 17:11:04 +0900

    admin 페이지 구현

--- src/main/java/kr/co/farmstory/controller/MainController.java
+++ src/main/java/kr/co/farmstory/controller/MainController.java
@@ -1,4 +1,20 @@
 package kr.co.farmstory.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
 public class MainController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String index(Authentication authentication, Model model){
+        return "/index";
+
+    }
 }
