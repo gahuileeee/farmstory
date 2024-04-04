5ff8e684d0ae610467130e522169fe00aa98691a
gahui446 <gahui446@gmail.com>
2024-04-04 17:25:28 +0900

    제발

--- src/main/java/kr/co/farmstory/dto/ArticlePageResponseDTO.java
+++ src/main/java/kr/co/farmstory/dto/ArticlePageResponseDTO.java
@@ -23,25 +23,6 @@ public class ArticlePageResponseDTO {
     private int start, end;
     private boolean prev, next;
 
-    @Builder
-    public ArticlePageResponseDTO(ArticlePageRequestDTO pageRequestDTO, List<ArticleDTO> dtoList, int total){
-        this.cate = pageRequestDTO.getCate();
-        this.type = pageRequestDTO.getType();
-        this.keyword = pageRequestDTO.getKeyword();
-        this.pg = pageRequestDTO.getPg();
-        this.size = pageRequestDTO.getSize();
-        this.total = total;
-        this.dtoList = dtoList;
-
-        this.startNo = total - ((pg - 1) * size);
-        this.end = (int) (Math.ceil(this.pg / 10.0)) * 10;
-        this.start = this.end - 9;
-
-        int last = (int) (Math.ceil(total / (double) size));
-        this.end = end > last ? last : end;
-        this.prev = this.start > 1;
-        this.next = total > this.end * this.size;
-    }
 
 
 
